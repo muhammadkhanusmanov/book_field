42a1ea6cae0c95b550ac0f7c8c87842462eb8018
username <email>
2024-05-30 19:19:10 +0500

    login/user/

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path,include
-from apis.views import CustomAPIView,RegisterUser
+from apis.views import CustomAPIView,RegisterUser,LoginUser
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/user/',RegisterUser.as_view()),
+    path('login/user/',LoginUser.as_view()),
     path('accounts/', include('allauth.urls')),
     path('accounts/google/login/callback/home/', CustomAPIView.as_view()),
     path('page/',CustomAPIView.as_view()),
