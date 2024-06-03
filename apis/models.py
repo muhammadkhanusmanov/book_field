93a4565b0ce0d3f3622be3e10706aa642cc70037
username <email>
2024-06-03 22:16:30 +0500

    prouser setting

--- apis/models.py
+++ apis/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class BasicModel(models.Model):
     created_ad = models.DateTimeField(auto_now_add=True)
@@ -8,16 +8,25 @@ class BasicModel(models.Model):
     class Meta:
         abstract = True
 
-
-
 class ProUser(AbstractUser):
     position_choices = [
-        ('admin','Admin'),
-        ('user','User'),
-        ('owner','Owner')
+        ('admin', 'Admin'),
+        ('user', 'User'),
+        ('owner', 'Owner')
     ]
     
-    position = models.CharField(max_length=10, choices=position_choices,default='user')
+    position = models.CharField(max_length=10, choices=position_choices, default='user')
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='pro_users',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='pro_users',
+        blank=True
+    )
 
     def __str__(self):
         return self.username
