4130365aa2844837643a45a1cd294ff91cf8cbc9
username <email>
2024-06-04 10:32:18 +0500

    begin

--- apis/models.py
+++ apis/models.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class BasicModel(models.Model):
     created_ad = models.DateTimeField(auto_now_add=True)
@@ -7,26 +7,3 @@ class BasicModel(models.Model):
     
     class Meta:
         abstract = True
-
-class ProUser(AbstractUser):
-    position_choices = [
-        ('admin', 'Admin'),
-        ('user', 'User'),
-        ('owner', 'Owner')
-    ]
-    
-    position = models.CharField(max_length=10, choices=position_choices, default='user')
-
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='pro_users',
-        blank=True
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='pro_users',
-        blank=True
-    )
-
-    def __str__(self):
-        return self.username
