59bf72d808da8644fe48c4e1f7d8a39cc6673e52
username <email>
2024-06-04 10:47:20 +0500

    position

--- apis/models.py
+++ apis/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 class BasicModel(models.Model):
     created_ad = models.DateTimeField(auto_now_add=True)
@@ -7,3 +7,16 @@ class BasicModel(models.Model):
     
     class Meta:
         abstract = True
+
+class UserPosition(BasicModel):
+    position_choices = [
+        ('admin','Admin'),
+        ('user','User'),
+        ('owner','Owner')
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='position')
+    position = models.CharField(max_length=6, choices=position_choices, default='user')
+    
+    def __str__(self):
+        return self.user.username
